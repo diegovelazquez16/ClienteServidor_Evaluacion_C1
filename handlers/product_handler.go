f9bf31ca424a8f3b74fad0f5ed033eb9719ffc1c
Diego Velazquez <govelazmen@gmail.com>
2025-02-04 15:19:04 -0600

    Add: servidor principal

--- handlers/product_handler.go
+++ handlers/product_handler.go
@@ -24,3 +24,59 @@ func CreateProduct(c *gin.Context) {
     c.JSON(http.StatusCreated, product)
 }
 
+func GetProduct(c *gin.Context) {
+    id, err := strconv.Atoi(c.Param("id"))
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid product ID"})
+        return
+    }
+
+    product, exists := products[id]
+    if !exists {
+        c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+        return
+    }
+
+    c.JSON(http.StatusOK, product)
+}
+
+func UpdateProduct(c *gin.Context) {
+    id, err := strconv.Atoi(c.Param("id"))
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid product ID"})
+        return
+    }
+
+    var updatedProduct models.Product
+    if err := c.ShouldBindJSON(&updatedProduct); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    if _, exists := products[id]; !exists {
+        c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+        return
+    }
+
+    updatedProduct.ID = id
+    products[id] = updatedProduct
+
+    c.JSON(http.StatusOK, updatedProduct)
+}
+
+func DeleteProduct(c *gin.Context) {
+    id, err := strconv.Atoi(c.Param("id"))
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid product ID"})
+        return
+    }
+
+    if _, exists := products[id]; !exists {
+        c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+        return
+    }
+
+    delete(products, id)
+    c.Status(http.StatusNoContent)
+}
+
--- main.go
+++ main.go
@@ -1,3 +1,37 @@
 package main
 
+import (
+    "log"
+    "github.com/gin-gonic/gin"
+    "t2/handlers"
+)
 
+func main() {
+    r := gin.Default()
+
+    r.POST("/products", handlers.CreateProduct)
+    r.GET("/products/:id", handlers.GetProduct)
+    r.PUT("/products/:id", handlers.UpdateProduct)
+    r.DELETE("/products/:id", handlers.DeleteProduct)
+
+
+    go func() {
+        log.Println("Main server started at :8080")
+        if err := r.Run(":8080"); err != nil {
+            log.Fatalf("Failed to start main server: %v", err)
+        }
+    }()
+
+    go func() {
+        replicationRouter := gin.Default()
+
+        log.Println("Replication server started at :8081")
+        if err := replicationRouter.Run(":8081"); err != nil {
+            log.Fatalf("Failed to start replication server: %v", err)
+        }
+    }()
+
+
+
+    select {}
+}
